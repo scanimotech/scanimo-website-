49aaf311aee8317e92d11b70735a42a32ecc8bb9
Sagarkanth19 <144892598+Sagarkanth19@users.noreply.github.com>
2025-10-26 16:51:18 +0530

    Update main.jsx

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
-import Contact_us from "./components/Contact_us.jsx";
-import OurTeam from './components/OurTeam.jsx';
-import Vedio from './components/Vedio.jsx';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
