bad3a16e5c40811a0c97ea355e99bc41518de031
Sagarkanth19 <144892598+Sagarkanth19@users.noreply.github.com>
2025-10-26 15:45:03 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
-import Contact_us from "./components/Contact_us.jsx";
-import OurTeam from './components/OurTeam.jsx';
-import Vedio from './components/vedio.jsx';
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      {/* <Footer1 /> */}
+  
       <App />  
-    {/* <OurTeam /> */}
-    {/* <Contact_us /> */}
+   
     
   </React.StrictMode>
 );
